eb66a847a2d0758f5cf871927a85285d000eab91
Brandon Ward <brandon.ward1993@gmail.com>
2021-01-10 10:17:20 -0700

    Write some unit tests around CORS configs

--- src/main/kotlin/com/ngenenius/api/config/ApplicationConfig.kt
+++ src/main/kotlin/com/ngenenius/api/config/ApplicationConfig.kt
@@ -5,15 +5,11 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
