06f8c46d0814c5d0684ee591e17db7254de0f8c8
Brandon Ward <brandon.ward1993@gmail.com>
2021-01-18 11:39:30 -0700

    NTV-11 compute twitch embed and chat urls in the api (#9)

--- src/main/kotlin/com/ngenenius/api/model/twitch/PublicTwitchObjects.kt
+++ src/main/kotlin/com/ngenenius/api/model/twitch/PublicTwitchObjects.kt
@@ -3,6 +3,8 @@
  */
 package com.ngenenius.api.model.twitch
 
+import com.fasterxml.jackson.annotation.JsonGetter
+
 data class AggregateTwitchResponse(
     /**
      * The Twitch user with basic details about their channel.
@@ -11,10 +13,29 @@ data class AggregateTwitchResponse(
     /**
      * Details about a current live stream.
      */
-    val stream: StreamDetails?
+    val stream: StreamDetails?,
+
+    /**
+     * The current index that this response has been placed.
+     * Some derived / synthetic content is driven off this index.
+     */
+    var index: Int = -1
 ) {
     /**
      * Whether or not the user is live streaming.
      */
     val live = stream != null
+
+    @JsonGetter
+    fun videoUrl(): String? {
+        val liveStream = stream ?: return null
+        val isPlaying = index == 0
+        return "https://player.twitch.tv/?channel=${liveStream.userName}&muted=${!isPlaying}&autoplay=${isPlaying}&parent={parent}"
+    }
+
+    @JsonGetter
+    fun chatUrl(): String? {
+        val liveStream = stream ?: return null
+        return "https://www.twitch.tv/embed/${liveStream.userName}/chat?parent={parent}&darkpopout"
+    }
 }
