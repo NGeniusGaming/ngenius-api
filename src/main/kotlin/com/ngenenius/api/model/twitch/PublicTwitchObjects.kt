677cdb3e471afb88edf2aa798b7678647ba4b384
Brandon Ward <brandon.ward1993@gmail.com>
2021-01-18 10:34:43 -0700

    NTV-11 adding synthetic 'live' flag to easily determine if a stream is in fact live

--- src/main/kotlin/com/ngenenius/api/model/twitch/PublicTwitchObjects.kt
+++ src/main/kotlin/com/ngenenius/api/model/twitch/PublicTwitchObjects.kt
@@ -4,6 +4,17 @@
 package com.ngenenius.api.model.twitch
 
 data class AggregateTwitchResponse(
+    /**
+     * The Twitch user with basic details about their channel.
+     */
     val user: UserDetails,
+    /**
+     * Details about a current live stream.
+     */
     val stream: StreamDetails?
-)
+) {
+    /**
+     * Whether or not the user is live streaming.
+     */
+    val live = stream != null
+}
